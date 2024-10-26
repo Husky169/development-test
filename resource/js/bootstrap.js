6db16cf1cf04552ef1ced03e49cf568633d63582
Husky169 <crs4wi@hotmail.com>
2024-10-26 18:33:56 +0200

    Add files via upload

---
+++ resource/js/bootstrap.js
@@ -0,0 +1,15 @@
+import * as htmx from 'htmx.org';
+window.htmx = htmx;
+
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+
+import Swiper from 'swiper';
+import { Autoplay, Thumbs } from 'swiper/modules';
+import 'swiper/css/bundle';
+window.Swiper = Swiper;
+window.SwiperAutoplay = Autoplay;
+window.SwiperThumbs = Thumbs;
+
+import Lenis from 'lenis';
+window.Lenis = Lenis;
