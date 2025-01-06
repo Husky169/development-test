fca394001db974c9f77b3e150d8a863840555f3e
Husky169 <crs4wi@hotmail.com>
2025-01-06 16:44:39 +0100

    Update particles2.js

--- resource/js/particles2.js
+++ resource/js/particles2.js
@@ -1,120 +1,124 @@
-      <div id="particles-js" class="absolute inset-0 z-[-1] h-full pointer-events-none"></div>
-    </div>
-
-    
-
-    <script src="https://cdn.jsdelivr.net/npm/alpinejs-masonry@1.0.15/dist/masonry.min.js" integrity="sha256-n3pRIWHXpKBr1FSI75nD5bgEoXDZ8/rS2k9/WHLVk2E=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
-    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.3/dist/cdn.min.js" integrity="sha256-aJ9ROXjRHWn00zeU9ylsmlhqLlXeebtEfN28P0dPnwc=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
-    <script src="https://cdn.jsdelivr.net/npm/smooth-zoom@1.4.1/dist/zoom.min.js" integrity="sha256-tDqoItrR4Xx5ox4AIBUmsQAF45Vp4wW2oy+HVOrqirI=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js" integrity="sha256-FZsW7H2V5X9TGinSjjwYJ419Xka27I8XPDmWryGlWtw=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
-    <script src="https://cdn.jsdelivr.net/npm/choices.js@10.2.0/public/assets/scripts/choices.min.js" integrity="sha256-P+JgcxEeZtxwYS1+TAAuusKFM646SB8Oodk0TYu9zuo=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
-    <script src="https://cdn.jsdelivr.net/npm/lite-youtube-embed@0.3.3/src/lite-yt-embed.js" integrity="sha256-t8Thbv2D6x0AC5rMk+Ydz589Zq4lp8ZC4iVfNNdxs5U=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
-    <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.js" integrity="sha256-icjghcPaibMf1jv4gQIGi5MeWNHem2SispcorCiCfSg=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
-    <script src="/assets/71057/script.js?v=1735065350147" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
-    <script type="f7c616513b06559fb1645171-text/javascript">
-      particlesJS('particles-js', {
-          'particles': {
-              'number': {
-                  'value': 20,
-                  'density': {
-                      'enable': true,
-                      'value_area': 800
-                  }
-              },
-              'color': {
-                  'value': "#e626ff",
-              },
-              'shape': {
-                  'type': 'circle',
-                  'stroke': {
-                      'width': 0,
-                      'color': '#000000'
-                  },
-                  'polygon': {
-                      'nb_sides': 5
-                  }
-              },
-                  'opacity': {
-                  'value': 0.8,
-                  'random': true,
-                  'anim': {
-                      'enable': false,
-                      'speed': 1,
-                      'opacity_min': 0.1,
-                      'sync': false
-                  }
-              },
-              'size': {
-                  'value': 10,
-                  'random': true,
-                  'anim': {
-                      'enable': false,
-                      'speed': 40,
-                      'size_min': 0.1,
-                      'sync': false
-                  }
-              },
-              'line_linked': {
-                  'enable': false,
-                  'distance': 500,
-                  'color': '#ffffff',
-                  'opacity': 0.4,
-                  'width': 2
-              },
-              'move': {
-                  'enable': true,
-                  'speed': 2,
-                  'direction': 'none',
-                  'random': true,
-                  'straight': false,
-                  'out_mode': 'out',
-                  'bounce': false,
-                  'attract': {
-                      'enable': false,
-                      'rotateX': 600,
-                      'rotateY': 1200
-                  }
-              }
-          },
-          'interactivity': {
-              'detect_on': 'canvas',
-              'events': {
-                  'onhover': {
-                      'enable': true,
-                      'mode': 'bubble'
-                  },
-                  'onclick': {
-                      'enable': true,
-                      'mode': 'repulse'
-                  },
-                  'resize': true
-              },
-              'modes': {
-                  'grab': {
-                      'distance': 400,
-                      'line_linked': {
-                      'opacity': 0.5
-                      }
-                  },
-                  'bubble': {
-                      'distance': 400,
-                      'size': 4,
-                      'duration': 0.3,
-                      'opacity': 1,
-                      'speed': 3
-                  },
-                  'repulse': {
-                      'distance': 200,
-                      'duration': 0.4
-                  },
-                  'push': {
-                      'particles_nb': 4
-                  },
-                  'remove': {
-                      'particles_nb': 2
-                  }
-              }
-          },
-          'retina_detect': true
-      });
-    </script>
+        <div id="particles-js" class="xlaqr xf5mc ryu0i nkaas qgb8l"></div>
+        <link rel="preload" as="style" href="./build/assets/app-Ck-h5v4U.css">
+        <link rel="modulepreload" href="./build/assets/app-Cu8h15KX.js">
+        <link rel="stylesheet" href="./build/assets/app-Ck-h5v4U.css" data-navigate-track="reload">
+        <script type="module" src="./build/assets/app-Cu8h15KX.js" data-navigate-track="reload"></script>
+        <script src="./build/assets/aos.min.js"></script>
+        <script>
+            AOS.init({
+                once: true,
+            });
+        </script>
+        <script src="./build/assets/particles.min.js"></script>
+        <script>
+            particlesJS("particles-js", {
+                particles: {
+                    number: {
+                        value: 40,
+                        density: {
+                            enable: true,
+                            value_area: 800
+                        }
+                    },
+                    color: {
+                        value: "#a200ff"
+                    },
+                    shape: {
+                        type: "circle",
+                        stroke: {
+                            width: 0,
+                            color: "#000000"
+                        },
+                        polygon: {
+                            nb_sides: 5
+                        },
+                        image: {
+                            src: "img/github.svg",
+                            width: 100,
+                            height: 100
+                        }
+                    },
+                    opacity: {
+                        value: 0.5,
+                        random: false,
+                        anim: {
+                            enable: false,
+                            speed: 1,
+                            opacity_min: 0.1,
+                            sync: false
+                        }
+                    },
+                    size: {
+                        value: 3,
+                        random: true,
+                        anim: {
+                            enable: false,
+                            speed: 40,
+                            size_min: 0.1,
+                            sync: false
+                        }
+                    },
+                    line_linked: {
+                        enable: true,
+                        distance: 150,
+                        color: "#ba75ff",
+                        opacity: 0.4,
+                        width: 1
+                    },
+                    move: {
+                        enable: true,
+                        speed: 1,
+                        direction: "none",
+                        random: false,
+                        straight: false,
+                        out_mode: "out",
+                        bounce: false,
+                        attract: {
+                            enable: false,
+                            rotateX: 600,
+                            rotateY: 1200
+                        }
+                    },
+                },
+                interactivity: {
+                    detect_on: "canvas",
+                    events: {
+                        onhover: {
+                            enable: true,
+                            mode: "repulse"
+                        },
+                        onclick: {
+                            enable: true,
+                            mode: "push"
+                        },
+                        resize: true
+                    },
+                    modes: {
+                        grab: {
+                            distance: 400,
+                            line_linked: {
+                                opacity: 1
+                            }
+                        },
+                        bubble: {
+                            distance: 400,
+                            size: 40,
+                            duration: 2,
+                            opacity: 8,
+                            speed: 3
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4
+                        },
+                        push: {
+                            particles_nb: 4
+                        },
+                        remove: {
+                            particles_nb: 2
+                        }
+                    }
+                },
+                retina_detect: true
+            });
+        </script>
