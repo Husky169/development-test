beb3c8fd4e4c315f0323f0ac0e4194dacd287dff
Husky169 <crs4wi@hotmail.com>
2025-01-06 16:42:22 +0100

    Update particles2.js

--- resource/js/particles2.js
+++ resource/js/particles2.js
@@ -1,3 +1,17 @@
+      <div id="particles-js" class="absolute inset-0 z-[-1] h-full pointer-events-none"></div>
+    </div>
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs-masonry@1.0.15/dist/masonry.min.js" integrity="sha256-n3pRIWHXpKBr1FSI75nD5bgEoXDZ8/rS2k9/WHLVk2E=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.3/dist/cdn.min.js" integrity="sha256-aJ9ROXjRHWn00zeU9ylsmlhqLlXeebtEfN28P0dPnwc=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/smooth-zoom@1.4.1/dist/zoom.min.js" integrity="sha256-tDqoItrR4Xx5ox4AIBUmsQAF45Vp4wW2oy+HVOrqirI=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js" integrity="sha256-FZsW7H2V5X9TGinSjjwYJ419Xka27I8XPDmWryGlWtw=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/choices.js@10.2.0/public/assets/scripts/choices.min.js" integrity="sha256-P+JgcxEeZtxwYS1+TAAuusKFM646SB8Oodk0TYu9zuo=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/lite-youtube-embed@0.3.3/src/lite-yt-embed.js" integrity="sha256-t8Thbv2D6x0AC5rMk+Ydz589Zq4lp8ZC4iVfNNdxs5U=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.js" integrity="sha256-icjghcPaibMf1jv4gQIGi5MeWNHem2SispcorCiCfSg=" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
+    <script src="/assets/71057/script.js?v=1735065350147" crossorigin="anonymous" type="f7c616513b06559fb1645171-text/javascript"></script>
+    <script type="f7c616513b06559fb1645171-text/javascript">
       particlesJS('particles-js', {
           'particles': {
               'number': {
